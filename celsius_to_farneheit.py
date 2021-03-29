66543c3c858940061bd54d5e60a1612008cc7ceb
Bharti1601 <81160310+Bharti1601@users.noreply.github.com>
2021-03-29 10:25:38 +0530

    Update celsius_to_farneheit.py

--- celsius_to_farneheit.py
+++ celsius_to_farneheit.py
@@ -1,5 +1,5 @@
 # Program to convert degree celcius into degree fahrenheit
 
-x = int(input("Enter the value in degree celcius : "))
-fahrenheit = (x*9/5)+32
-print("The result will be", fahrenheit , "fahrenheit")
+x = int(input("Enter the value in degree celcius : ")) # take integer as an input and store it ino x variable 
+fahrenheit = (x*9/5)+32  # coverts the celcius value into farneheit value 
+print("The result will be", fahrenheit , "fahrenheit")  # print the value which comes in farneheit variable
